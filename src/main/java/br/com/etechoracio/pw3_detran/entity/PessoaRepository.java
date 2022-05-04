9ae620ebcb0eceaabdaf088c70065c88f034648b
allan-faoro <allan-faoro@test.com>
2022-05-04 14:36:08 -0300

    Criacao do PessoaRepository

---
+++ src/main/java/br/com/etechoracio/pw3_detran/entity/PessoaRepository.java
@@ -0,0 +1,7 @@
+package br.com.etechoracio.pw3_detran.entity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PessoaRepository extends JpaRepository <Pessoa, Integer>{
+
+}
